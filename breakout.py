a09830237090b23aaf18b82e57b0e3550a7f2747
Jeremy Nguyen <jn042@hdm-stuttgart.de>
2024-04-15 17:56:00 +0200

    moved some code lines into functions for cleaner breakout game

--- breakout.py
+++ breakout.py
@@ -1,106 +1,102 @@
-import pygame
 import random
 import settings
 from paddle import Paddle
 from brick import Brick, HardBrick, SpecialBrick
 from ball import Ball
 from counter import Counter
+from settings import *
 
 
-# Initialize the PyGame
-pygame.mixer.pre_init(44100, -16, 2, 512)
-pygame.init()
+def initialize():
+    # Initialize the PyGame
+    pygame.mixer.pre_init(44100, -16, 2, 512)
+    pygame.init()
 
-# Screen setup
-screen = pygame.display.set_mode((700, 500))
-pygame.display.set_caption("Breakout")
+    # Screen setup
+    screen = pygame.display.set_mode((700, 500))
+    pygame.display.set_caption("Breakout")
 
-# Clock setup
-clock = pygame.time.Clock()
-FPS = 60
+    # Clock setup
+    clock = pygame.time.Clock()
+    FPS = 60
 
-# Colors
-BLACK = pygame.Color("black")
-WHITE = pygame.Color("snow")
-RED = pygame.Color("red4")
-ORANGE = pygame.Color("coral")
-GREEN = pygame.Color("green4")
-YELLOW = pygame.Color("yellow")
-BLUE = pygame.Color("skyblue1")
-PINK = pygame.Color("hotpink4")
-GREY = pygame.Color("grey50")
+    # Fonts
+    MESSAGE_FONT = pygame.font.Font("squarefont.ttf", 50)
+    INSTRUCTION_FONT = pygame.font.Font("squarefont.ttf", 30)
 
-# Fonts
-MESSAGE_FONT = pygame.font.Font("squarefont.ttf", 50)
-INSTRUCTION_FONT = pygame.font.Font("squarefont.ttf", 30)
+    # Start message
+    start_message = MESSAGE_FONT.render("HIT SPACE TO START THE BALL", True, BLUE)
+    control_message = INSTRUCTION_FONT.render("A/D TO MOVE LEFT/RIGHT", True, BLUE)
+    music_message = INSTRUCTION_FONT.render("M TO TOGGLE MUSIC", True, BLUE)
+    fx_message = INSTRUCTION_FONT.render("N TO TOGGLE SOUND", True, BLUE)
 
-# Start message
-start_message = MESSAGE_FONT.render("HIT SPACE TO START THE BALL", True, BLUE)
-control_message = INSTRUCTION_FONT.render("A/D TO MOVE LEFT/RIGHT", True, BLUE)
-music_message = INSTRUCTION_FONT.render("M TO TOGGLE MUSIC", True, BLUE)
-fx_message = INSTRUCTION_FONT.render("N TO TOGGLE SOUND", True, BLUE)
+    # Music
+    pygame.mixer.music.load("breakout-music.ogg")
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.1)
 
-BRICK_HEIGHT = 15
-BRICK_WIDTH = 50
-BRICK_MARGIN = 0
-BRICK_BORDER = 0
-BRICK_TOP = 100
+    return screen, clock, FPS, BLACK, WHITE, RED, ORANGE, GREEN, YELLOW, BLUE, PINK, GREY, MESSAGE_FONT, INSTRUCTION_FONT, start_message, control_message, music_message, fx_message
 
-ROW_COLORS = [RED, RED, ORANGE, ORANGE, GREEN, GREEN, YELLOW, YELLOW]
+def generate_level_layout(total_bricks, hard_brick_percentage):
+    n_hard_bricks = int(total_bricks * hard_brick_percentage)
+    brick_types = ['hard'] * n_hard_bricks
+    brick_types.extend(['normal'] * (total_bricks - n_hard_bricks))
+    random.shuffle(brick_types)
+    brick_types[random.randint(-14, -1)] = "special"
+    return brick_types
 
-HARD_BRICK_COLOR = GREY
-SPECIAL_BRICK_COLOR = PINK
-# HARD_BRICK_PERCENTAGE = 20
+def build_brick_wall(bricks, brick_types, N_ROWS, N_COLUMNS, BRICK_BORDER, BRICK_WIDTH, BRICK_MARGIN, BRICK_HEIGHT, BRICK_TOP, ROW_COLORS, HARD_BRICK_COLOR, SPECIAL_BRICK_COLOR):
+    index = 0
+    for row in range(N_ROWS):
+        for column in range(N_COLUMNS):
+            x = BRICK_BORDER + column * (BRICK_WIDTH + 2 * BRICK_MARGIN) + BRICK_MARGIN
+            y = BRICK_TOP + row * (BRICK_HEIGHT + 2 * BRICK_MARGIN) + BRICK_MARGIN
+            if brick_types[index] == 'hard':
+                brick = SpecialBrick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), HARD_BRICK_COLOR)
+            elif brick_types[index] == 'normal':
+                brick = Brick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), ROW_COLORS[row])
+            elif brick_types[index] == 'special':
+                brick = SpecialBrick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), SPECIAL_BRICK_COLOR)
+            index += 1
+            bricks.add(brick)
+    for brick in bricks:
+        brick.init_neighbours(bricks)
+
+def update_game(dt, paddles, balls, bricks):
+    # Update
+    paddles.update(dt)
+    balls.update(dt)
+    bricks.update(dt)
+
+def draw_game(screen, background, paddles, bricks, balls, score, lives, start_message, control_message, music_message, fx_message):
+    # Draw
+    screen.blit(background, (0, 0))
+    paddles.draw(screen)
+    bricks.draw(screen)
+    balls.draw(screen)
+    score.draw(screen)
+    lives.draw(screen)
+
+screen, clock, FPS, BLACK, WHITE, RED, ORANGE, GREEN, YELLOW, BLUE, PINK, GREY, MESSAGE_FONT, INSTRUCTION_FONT, start_message, control_message, music_message, fx_message = initialize()
+
+
 # Sprites
 paddles = pygame.sprite.Group()
 balls = pygame.sprite.Group()
 bricks = pygame.sprite.Group()
 
 Paddle.PADDLE_HEIGHT = BRICK_HEIGHT
-paddle = Paddle((350, 450), 1000, 400, BLUE)
+paddle = Paddle((350, 450), 200, 400, BLUE)
 paddles.add(paddle)
-'''
-for row in range(8):
-    for column in range(14):
-        x = BRICK_BORDER + column * (BRICK_WIDTH + 2 * BRICK_MARGIN) + BRICK_MARGIN
-        y = BRICK_TOP + row * (BRICK_HEIGHT + 2 * BRICK_MARGIN) + BRICK_MARGIN
-        if random.randint(1, 100) <= HARD_BRICK_PERCENTAGE:
-            brick = HardBrick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), HARD_BRICK_COLOR)
-        else:
-            brick = Brick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), ROW_COLORS[row])
 
-        # Add brick to the group
-        bricks.add(brick)
-'''
 N_ROWS = 8
 N_COLUMNS = 14
 TOTAL_BRICKS = N_ROWS * N_COLUMNS
 HARD_BRICK_PERCENTAGE = 0.2
 
-n_hard_bricks = int(TOTAL_BRICKS * HARD_BRICK_PERCENTAGE)
-brick_types = []
+brick_types = generate_level_layout(TOTAL_BRICKS, HARD_BRICK_PERCENTAGE)
 
-for i in range(n_hard_bricks):
-    brick_types.append('hard')
-brick_types.extend(['normal'] * (TOTAL_BRICKS - n_hard_bricks))
-random.shuffle(brick_types)
-brick_types[random.randint(-14, -1)] = "special"
-
-index = 0
-for row in range(N_ROWS):
-    for column in range(N_COLUMNS):
-        x = BRICK_BORDER + column * (BRICK_WIDTH + 2 * BRICK_MARGIN) + BRICK_MARGIN
-        y = BRICK_TOP + row * (BRICK_HEIGHT + 2 * BRICK_MARGIN) + BRICK_MARGIN
-        if brick_types[index] == 'hard':
-            brick = HardBrick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), HARD_BRICK_COLOR)
-        elif brick_types[index] == 'normal':
-            brick = Brick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), ROW_COLORS[row])
-        elif brick_types[index] == 'special':
-            brick = SpecialBrick((x, y), (BRICK_WIDTH, BRICK_HEIGHT), SPECIAL_BRICK_COLOR)
-        index += 1
-        bricks.add(brick)
-for brick in bricks:
-    brick.init_neighbours(bricks)
+build_brick_wall(bricks, brick_types, N_ROWS, N_COLUMNS, BRICK_BORDER, BRICK_WIDTH, BRICK_MARGIN, BRICK_HEIGHT, BRICK_TOP, ROW_COLORS, HARD_BRICK_COLOR, SPECIAL_BRICK_COLOR)
 
 # Counters
 score = Counter((10, 10), "squarefont.ttf", 80, WHITE)
@@ -110,12 +106,6 @@ lives.set_value(5)
 # Background
 background = pygame.Surface(screen.get_rect().size)
 background.fill(BLACK)
-
-# Music
-pygame.mixer.music.load("breakout-music.ogg")
-pygame.mixer.music.play(-1)
-
-pygame.mixer.music.set_volume(0.5)
 
 # Flag for sound effect mute
 muted = True
@@ -152,22 +142,14 @@ while running:
         elif event.type == Brick.BRICK_DESTROYED:
             score.change_value(event.value)
         elif event.type == Ball.BALL_LOST:
-            lives.change_value(-1)
-            if lives.get_value() == 0:
-                running = False
+            if len(balls) == 0:
+                lives.change_value(-1)
+                if lives.get_value() == 0:
+                    running = False
 
-    # Update
-    paddles.update(dt)
-    balls.update(dt)
-    bricks.update(dt)
-
-    # Draw
-    screen.blit(background, (0, 0))
-    paddles.draw(screen)
-    bricks.draw(screen)
-    balls.draw(screen)
-    score.draw(screen)
-    lives.draw(screen)
+    update_game(dt, paddles, balls, bricks)
+    draw_game(screen, background, paddles, bricks, balls, score, lives, start_message, control_message, music_message,
+              fx_message)
 
     # Show start message if there is no ball
     if not len(balls):
--- settings.py
+++ settings.py
@@ -1,1 +1,31 @@
+import pygame
+
+# Game configuration
+FPS = 60
+BRICK_HEIGHT = 15
+BRICK_WIDTH = 50
+BRICK_MARGIN = 0
+BRICK_BORDER = 0
+BRICK_TOP = 100
+N_ROWS = 8
+N_COLUMNS = 14
+TOTAL_BRICKS = N_ROWS * N_COLUMNS
+HARD_BRICK_PERCENTAGE = 0.2
+PADDLE_HEIGHT = BRICK_HEIGHT
+
+# Colors
+BLACK = pygame.Color("black")
+WHITE = pygame.Color("snow")
+RED = pygame.Color("red4")
+ORANGE = pygame.Color("coral")
+GREEN = pygame.Color("green4")
+YELLOW = pygame.Color("yellow")
+BLUE = pygame.Color("skyblue1")
+PINK = pygame.Color("hotpink4")
+GREY = pygame.Color("grey50")
+ROW_COLORS = [RED, RED, ORANGE, ORANGE, GREEN, GREEN, YELLOW, YELLOW]
+HARD_BRICK_COLOR = GREY
+SPECIAL_BRICK_COLOR = PINK
+
+# Sound
 sfx_muted = False
